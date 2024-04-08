d75af6e22731297658e0f47ed6e3e3e553ce3a09
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-08 15:17:39 -0700

    updating connection to mongodb to use cloud atlas service

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
+
+EuD4GkJZuLaWcc72
