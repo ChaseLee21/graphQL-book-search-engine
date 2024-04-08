eda3ad7765fbf236ef90794695451245982fdaf5
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-08 16:13:10 -0700

    updating connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: '../../.env'});
-
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
