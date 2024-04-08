45e707d6b7ded5c62904e5f52ab46da6d5581aa5
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-08 13:02:44 -0700

    added LOGIN_USER and REMOVE_BOOK to gql mutations util

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -22,9 +22,50 @@ export const ADD_USER = gql`
     }
 `;
 
+export const LOGIN_USER = gql`
+    mutation Login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+            }
+        }
+        }
+    }
+`;
+
 export const SAVE_BOOK = gql`
     mutation Mutation($bookData: BookInput!, $userId: ID!) {
         saveBook(bookData: $bookData, userId: $userId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation Mutation($bookId: ID!, $userId: ID!) {
+        removeBook(bookId: $bookId, userId: $userId) {
         _id
         username
         email
