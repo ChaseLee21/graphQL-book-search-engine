ed6e996c153708c1c7dba33c29947cced7c99b95
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-08 19:07:28 -0700

    updating connection code in attempt to fix an error connecting to the atlas remote db

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const uri = process.env.MONGODB_URI;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+    try {
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(uri, clientOptions);
+    } catch (error) {
+        console.error(error);
+    }
+}
+run().catch(console.dir);
 
 module.exports = mongoose.connection;
